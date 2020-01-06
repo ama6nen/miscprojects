10e7d3adcc3006d87e552ae85ebb80f31f59b3a9
ama <ama6nen@gmail.com>
2020-01-06 17:38:15 +0200

    Discord usage limiter
    Discord usage limiter which only allows you to use discord when you have done productive things

---
+++ dclimit/dclimit/mainForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace dclimit
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.WorkButton = new System.Windows.Forms.Button();
+            this.DiscordButton = new System.Windows.Forms.Button();
+            this.DiscordTime = new System.Windows.Forms.Label();
+            this.Watchdog = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // WorkButton
+            // 
+            this.WorkButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.WorkButton.ForeColor = System.Drawing.Color.LawnGreen;
+            this.WorkButton.Location = new System.Drawing.Point(12, 13);
+            this.WorkButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.WorkButton.Name = "WorkButton";
+            this.WorkButton.Size = new System.Drawing.Size(121, 30);
+            this.WorkButton.TabIndex = 0;
+            this.WorkButton.Text = "Start work mode";
+            this.WorkButton.UseVisualStyleBackColor = false;
+            this.WorkButton.Click += new System.EventHandler(this.WorkButton_Click);
+            // 
+            // DiscordButton
+            // 
+            this.DiscordButton.Location = new System.Drawing.Point(141, 13);
+            this.DiscordButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.DiscordButton.Name = "DiscordButton";
+            this.DiscordButton.Size = new System.Drawing.Size(121, 30);
+            this.DiscordButton.TabIndex = 1;
+            this.DiscordButton.Text = "Discord mode";
+            this.DiscordButton.UseVisualStyleBackColor = true;
+            this.DiscordButton.Click += new System.EventHandler(this.DiscordButton_Click);
+            // 
+            // DiscordTime
+            // 
+            this.DiscordTime.AutoSize = true;
+            this.DiscordTime.Location = new System.Drawing.Point(13, 52);
+            this.DiscordTime.Name = "DiscordTime";
+            this.DiscordTime.Size = new System.Drawing.Size(195, 17);
+            this.DiscordTime.TabIndex = 2;
+            this.DiscordTime.Text = "Allowed Discord time: 0 minutes";
+            // 
+            // Watchdog
+            // 
+            this.Watchdog.Enabled = true;
+            this.Watchdog.Interval = 1000;
+            this.Watchdog.Tick += new System.EventHandler(this.Watchdog_Tick);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 77);
+            this.ControlBox = false;
+            this.Controls.Add(this.DiscordTime);
+            this.Controls.Add(this.DiscordButton);
+            this.Controls.Add(this.WorkButton);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Discord limiter";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button WorkButton;
+        private System.Windows.Forms.Button DiscordButton;
+        private System.Windows.Forms.Label DiscordTime;
+        private System.Windows.Forms.Timer Watchdog;
+    }
+}
+
---
+++ dclimit/dclimit/mainForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dclimit
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool IsIconic(IntPtr hWnd);
+
+        int discordPoints = 1;
+        int fractionalCounter = 0;
+        DateTime WorkStartTime = default;
+        private void Watchdog_Tick(object sender, EventArgs e)
+        {
+            if (discordPoints < 1)
+            {
+                if (!WorkButton.Enabled && DiscordButton.Enabled)
+                    WorkButton.Enabled = true;
+                DiscordButton.Enabled = false;
+            }
+
+            bool discordMode = !WorkButton.Enabled;
+            if (discordPoints == 1)
+                DiscordTime.Text = $"Allowed DC time: {60 - fractionalCounter} seconds";
+            else
+                DiscordTime.Text = $"Allowed DC time: {discordPoints - 1} min {60 - fractionalCounter} sec";
+
+            if (discordMode)
+            {
+                fractionalCounter++;
+                if (fractionalCounter >= 60)
+                {
+                    discordPoints--;
+                    fractionalCounter = 0;
+                }
+                if (discordPoints > 0)
+                    return;
+            }
+            var procs = Process.GetProcessesByName("Discord");
+            foreach (var proc in procs)
+                if (!string.IsNullOrEmpty(proc.MainWindowTitle))
+                    if (!IsIconic(proc.MainWindowHandle))
+                        ShowWindow(proc.MainWindowHandle, 6);
+        }
+
+        private void WorkButton_Click(object sender, EventArgs e)
+        {
+            bool startNow = WorkButton.Text.StartsWith("Start");
+            if (startNow)
+            {
+                WorkButton.Text = WorkButton.Text.Replace("Start", "End");
+                WorkButton.ForeColor = Color.Red;
+                WorkStartTime = DateTime.Now;
+            }
+            else
+            {
+                var elapsed = DateTime.Now.Subtract(WorkStartTime);
+                var elapsedPoints = elapsed.TotalMinutes / 6.0;
+                var minutes = Convert.ToInt32(elapsedPoints);
+                discordPoints += minutes;
+                if(fractionalCounter > 0)
+                {
+                    var val = Convert.ToInt32(100.0 * (elapsedPoints - Math.Floor(elapsedPoints)));
+                    fractionalCounter -= val;
+                    if (fractionalCounter < 0)
+                        fractionalCounter = 0;
+                }
+                WorkButton.Text = WorkButton.Text.Replace("End", "Start");
+                WorkButton.ForeColor = Color.LawnGreen;
+            }
+            DiscordButton.Enabled = !startNow;
+        }
+
+        private void DiscordButton_Click(object sender, EventArgs e)
+        {
+            bool startNow = DiscordButton.Text.StartsWith("Discord");
+            if (startNow)
+                DiscordButton.Text = DiscordButton.Text.Replace("Discord", "No discord");
+            else
+                DiscordButton.Text = DiscordButton.Text.Replace("No discord", "Discord");
+
+            WorkButton.Enabled = !startNow;
+        }
+    }
+}
