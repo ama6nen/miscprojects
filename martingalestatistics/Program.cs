6e152d95dc2b99385d5b83cc7efa3dc0b0c2b45d
ama <ama6nen@gmail.com>
2020-01-10 14:33:18 +0200

    full refactor and add some some new logging

--- martingalestatistics/Program.cs
+++ martingalestatistics/Program.cs
@@ -9,138 +9,139 @@ namespace martingale_statistics
 {
     class Program
     {
-        static void Main( string[ ] args )
+
+        public class MartingaleInfo
         {
-           ulong[] asd()
+            public long time_elapsed = default;
+            public ulong wins = default;
+            public ulong losses = default;
+            public ulong max_lose_streak = default;
+            public ulong max_win_streak = default;
+            public ulong max_bet = default;
+
+            public static MartingaleInfo operator +(MartingaleInfo info1, MartingaleInfo info2)
             {
+                info1.time_elapsed += info2.time_elapsed;
+                info1.wins += info2.wins;
+                info1.losses += info2.losses;
+                info1.max_lose_streak += info2.max_lose_streak;
+                info1.max_win_streak += info2.max_win_streak;
+                info1.max_bet += info2.max_bet;
+                return info1;
+            }
 
+            public static MartingaleInfo operator /(MartingaleInfo info, ulong dividor)
+            {
+                info.time_elapsed /= (long)dividor;
+                info.wins /= dividor;
+                info.losses /= dividor; 
+                info.max_lose_streak /= dividor;
+                info.max_win_streak /= dividor;
+                info.max_bet /= dividor;
+                return info;
+            }
+        }
 
-                ulong money = 1_000_00; // 100k 
-                ulong basebet = 1;
+        public static MartingaleInfo RunSimulation(ulong money = 100_000, ulong basebet = 1)
+        {
+            var info = new MartingaleInfo();
 
-             
-                    
-                ulong losestreak = 0;
-                ulong currentbet = basebet;
-                ulong maxbet = 0;
-                ulong winstreak = 0;
-                Random random = new Random( );
-                ulong losses = 0;
-                ulong wins = 0;
-                bool didWinLast = false;
-                ulong currlose = 0;
-                ulong currwin = 0;
-                Stopwatch watch = new Stopwatch( );
-                watch.Start( );
-                while (money > 0)
+            var watch = new Stopwatch();
+            watch.Start();
+
+            var random = new Random();
+            ulong current_bet = basebet;
+            ulong start_money = money;
+            bool won_last = false;
+            ulong current_loss_streak = 0;
+            ulong current_win_streak = 0;
+
+            ulong multiplier = 2;
+            ulong last_multiplier = 0;
+
+            while (money > 0)
+            {
+                int rand = random.Next(0, 100);
+                if (current_bet > info.max_bet)
+                    info.max_bet = current_bet;
+                
+
+                if(last_multiplier != multiplier && (money / multiplier) > start_money)
                 {
-                    int rand = random.Next( 0 , 100 );
-                    if (money > 1_500_000)
-                        Console.WriteLine( "more" );
-
-                        if (currentbet > maxbet)
-                    {
-                        //  Console.WriteLine( "new biggest bet " + currentbet );
-                        maxbet = currentbet;
-                    }
-
-                    if (maxbet > money)
-                    {
-                        //  Console.WriteLine( "h" );
-                        break;
-                    }
-
-
-                    if (rand >= 50) // win
-                    {
-
-                        if (didWinLast)
-                            currwin++;
-                        else
-                        {
-                            if (currlose > losestreak)
-                            {
-                                //         Console.WriteLine( "new max lose streak " + currlose );
-                                losestreak = currlose;
-                                currlose = 0;
-                            }
-                        }
-                        wins++;
-                        money += currentbet;
-                        didWinLast = true;
-                        currentbet = 1;
-                    }
-                    else //lose
-                    {
-
-                        if (didWinLast)
-                        {
-                            if (currwin > winstreak)
-                            {
-                                //      Console.WriteLine( "new max win streak " + currwin );
-                                winstreak = currwin;
-                                currwin = 0;
-                            }
-                        }
-                        else
-                            currlose++;
-
-
-                        losses++;
-                        didWinLast = false;
-                        money -= currentbet;
-                        currentbet *= 2;
-                    }
+                    Console.WriteLine("Funds exceed " + multiplier + " times the initial money!");
+                    last_multiplier = multiplier;
+                    multiplier++;
                 }
+                
+                if (info.max_bet > money) //not enough money
+                    break;
 
-                watch.Stop( );
-                //Console.WriteLine( "time elapsed in ms  " + watch.ElapsedMilliseconds );
-                //Console.WriteLine( "out of money!" );
-                //Console.WriteLine( "total wins " + wins );
-                //Console.WriteLine( "total losses " + losses );
-                //Console.WriteLine( "biggest losing streak " + losestreak );
-                //Console.WriteLine( "biggest winning streak " + winstreak );
-                //Console.WriteLine( "max bet " + maxbet );
+                if (rand >= 50) // win
+                {
+                    if (won_last)
+                        current_win_streak++;
+                    else
+                    {
+                        if (current_loss_streak > info.max_lose_streak)
+                        {
+                            info.max_lose_streak = current_loss_streak;
+                            current_loss_streak = 0;
+                        }
+                    }
+                    info.wins++;
+                    money += current_bet;
+                    won_last = true;
+                    current_bet = basebet;
+                }
+                else //lose
+                {
 
-                return new ulong [ ]{ (ulong)watch.ElapsedMilliseconds, wins, losses, losestreak, winstreak, maxbet};
-                //   Console.ReadKey( );
+                    if (won_last)
+                    {
+                        if (current_win_streak > info.max_win_streak)
+                        {
+                            info.max_win_streak = current_win_streak;
+                            current_win_streak = 0;
+                        }
+                    }
+                    else
+                        current_loss_streak++;
+
+                    info.losses++;
+                    won_last = false;
+                    money -= current_bet;
+                    current_bet *= 2;
+                }
             }
-            double elapsed2 = 0;
-            double wins2 = 0;
-            double losses2 = 0;
-            double losestreak2 = 0;
-            double winstreak2 = 0;
-            double maxbet2 = 0;
-            var b = asd( );
-            elapsed2 = (elapsed2 + b[ 0 ]) / 2d;
-            wins2 = (wins2 + b[ 1 ]) / 2d;
-            losses2 = (losses2 + b[ 2 ]) / 2d;
-            losestreak2 = (losestreak2 + b[ 3 ]) / 2d;
-            winstreak2 = (winstreak2 + b[ 4 ]) / 2d;
-            maxbet2 = (maxbet2 + b[ 5 ]) / 2d;
-            for (int i =0; i < 1; i++)
+            watch.Stop();
+            info.time_elapsed = watch.ElapsedMilliseconds;
+            return info;
+        }
+
+        public static MartingaleInfo RunManySimulations(ulong money = 100_000, ulong basebet = 1, ulong samples = 5)
+        {
+            var info = new MartingaleInfo();
+            for (ulong i = 0; i < samples; i++)
             {
-                var a = asd( );
-                elapsed2 = (elapsed2 + a[ 0]) / 2d;
-                wins2 = (wins2 + a[ 1 ]) / 2d;
-                losses2 = (losses2 + a[ 2 ]) / 2d;
-                losestreak2 = (losestreak2 + a[ 3 ]) / 2d;
-                winstreak2 = (winstreak2 + a[ 4 ]) / 2d;
-                maxbet2 = (maxbet2 + a[ 5]) / 2d;
-
-
+                Console.WriteLine("Running simulation n" + i);
+                info += RunSimulation(money, basebet);
             }
-
-            Console.WriteLine( "[AVERAGES]" );
-            Console.WriteLine( "time elapsed in ms  " + elapsed2 );
-            Console.WriteLine( "total wins " + wins2 );
-            Console.WriteLine( "total losses " + losses2 );
-            Console.WriteLine( "biggest losing streak " + losestreak2 );
-            Console.WriteLine( "biggest winning streak " + winstreak2 );
-            Console.WriteLine( "max bet " + maxbet2 );
-            Console.ReadKey( );
-
-
+                
+            Console.WriteLine("total time elapsed (not average): " + info.time_elapsed);
+            info /= samples;
+            return info;
+        }
+        static void Main(string[] args)
+        {
+            var result = RunManySimulations(300_000, 1, 10);
+            Console.WriteLine("[AVERAGES]");
+            Console.WriteLine("time elapsed in ms: " + result.time_elapsed);
+            Console.WriteLine("total wins: " + result.wins);
+            Console.WriteLine("total losses: " + result.losses);
+            Console.WriteLine("biggest losing streak: " + result.max_lose_streak);
+            Console.WriteLine("biggest winning streak: " + result.max_win_streak);
+            Console.WriteLine("max bet: " + result.max_bet);
+            Console.ReadKey();
         }
     }
 }
